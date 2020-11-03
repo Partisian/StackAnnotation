9d35cde3653996ddce11cebe549142a4e68eddcc
Partisian <73816510+Partisian@users.noreply.github.com>
2020-11-03 15:24:41 +0800

    Update MyStringStackTest.java
    force build fail gtc

--- test/test/MyStringStackTest.java
+++ test/test/MyStringStackTest.java
@@ -123,7 +123,7 @@ public class MyStringStackTest extends TestCase {
         String o1 = null;  
         MyStringStack stack = new MyStringStack();
         assertTrue (stack.isEmpty());
-        stack.push (s1);
+        stack.push (s1)
         try {
             o1 = stack.pop ();
             assertSame (s1, o1);
