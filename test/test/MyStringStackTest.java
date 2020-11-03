8b0afed06a521cf2a76c973255bac86569520104
Partisian <73816510+Partisian@users.noreply.github.com>
2020-11-03 14:53:07 +0800

    Update MyStringStackTest.java
    fail this build GTC

--- test/test/MyStringStackTest.java
+++ test/test/MyStringStackTest.java
@@ -121,9 +121,9 @@ public class MyStringStackTest extends TestCase {
 	@Test
 	public void testStackSequence() {
         String o1 = null;  
-        //MyStringStack stack = new MyStringStack();
-        //assertTrue (stack.isEmpty());
-        //stack.push (s1);
+        MyStringStack stack = new MyStringStack();
+        assertTrue (stack.isEmpty());
+        stack.push (s1);
 		MyStringStack stack = new MyStringStack();
 		stack.push (s1);
 		assertTrue (stack.isEmpty());
